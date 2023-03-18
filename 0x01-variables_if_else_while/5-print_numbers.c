f2ad3950e83c80f4c82ed7d652ee834852377c24
Rahma <rimo@example.com>
2023-03-17 18:52:49 -0700

    new

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
  /**
   * main -Entry point
-  *
+  *Description: loop numbers by for loop.
   * Return: Always 0 (Success)
   */
 
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		printf("%d", num);
 	putchar('\n');
