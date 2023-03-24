edac91b512e9845c9cbdc36c395e32584b3d1dea
Rahma <rimo@example.com>
2023-03-23 19:30:50 -0700

    the first group of tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul  - muliplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: retun result
+ */
+int  mul(int a, int b)
+{
+	return (a * b);
+}
