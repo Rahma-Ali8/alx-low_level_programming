fd923c44e0b8a46cae54b1fae1b29206cc168c78
Rahma <rimo@example.com>
2023-03-24 14:13:29 -0700

    last task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,11 @@
 #include "main.h"
-/***/
+/**
+ * print_triangle - print a triangle by # sign
+ *
+ * @size: the size of the triangle
+ *
+ * Return: Always 0 (Success)
+*/
 
 void print_triangle(int size)
 {
@@ -9,7 +15,7 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (l = 1; l<= size; l++)
+		for (l = 1; l <= size; l++)
 		{
 			for (b = 1; b <= size; b++)
 			{
