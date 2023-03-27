682cd203971a238922b1e25a7494709218535ec3
Rahma <rimo@example.com>
2023-03-27 15:59:37 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 -  prints every other character of a string,
+ * puts_half -  prints every other character of a string,
  *		starting with the first character,
  *		followed by a new line
  * @str: string input
@@ -8,7 +8,7 @@
  * Return: Nothing
 */
 
-void puts2(char *str)
+void puts_half(char *str)
 {
 	int i;
 
