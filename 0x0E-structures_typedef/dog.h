711ff1b2836de8eae4ca44591fbc2d3ba7a4932f
Rahma <rimo@example.com>
2023-04-18 08:14:30 -0700

    1 , 2

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
