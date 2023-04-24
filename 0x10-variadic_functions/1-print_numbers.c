fc3210bc929d3a545281f77150eede7c281bc430
Rahma <rimo@example.com>
2023-04-24 13:23:20 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,13 @@
 #include "variadic_functions.h"
 
 /**
- * print_number - */
+ * print_number - function that prints numbers, followed by a new line
+ * @separator: string separator
+ * @n: number of arg
+ * @...: int to print
+ *
+ * Return: void
+*/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int i = n;
@@ -15,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(po, n);
 
 	while (i--)
-		printf("%d%S", va_arg(po, int),
+		printf("%d%S", va_arg(po, int), 
 				i ? (separator ? separator : "") : "\n");
 	va_end(po);
 
