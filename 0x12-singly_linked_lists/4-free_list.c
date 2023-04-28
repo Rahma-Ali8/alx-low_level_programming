6e5b7c826a3844d9a2f01e35e17698ec763a554e
Rahma <rimo@example.com>
2023-04-28 16:04:42 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer
+ *
+ * Return: Nothing
+*/
+
+void free_list(list_t *head)
+{
+	list_t *node, next_node;
+
+	if (!head)
+		return;
+	node = head;
+
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node->next);
+		node = next_node;
+	}
+}
