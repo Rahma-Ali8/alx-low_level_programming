4225f88e21a1e7592ae5d8eb98df152d2e662e35
Rahma <rimo@example.com>
2023-05-08 15:37:38 -0700

    write a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: file to create
+ * @text_content: pointer to write the file
+ *
+ * Return: 0 on success
+ * -1 on failed
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
