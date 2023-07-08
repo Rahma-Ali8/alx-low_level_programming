c0925718f3589a2e550aa28b6778fd8e0e9e9e3c
Rahma Ali <rimo@gmail.com>
2023-07-08 05:12:51 -0700

    convert bin to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,6 +10,7 @@
 */
 
 unsigned int binary_to_uint(const char *b)
+{
 
 	int slen = strlen(b);
 	int sum = 0;
@@ -21,11 +22,11 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if ((b[i] != '1') && (b[i] != '0'))
 			return (0);
-		if (b[i] == '1')sum += num;
+		if (b[i] == '1')
+			sum += num;
 		num *= 2;
 
 
-	
 	}
 	return (sum);
 
